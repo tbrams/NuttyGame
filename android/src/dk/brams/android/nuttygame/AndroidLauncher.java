1a0735ec8a069e166493a33acb8cf8a7e3c71e0f
Torben Brams <torben@brams.dk>
2016-06-24 17:42:21 +0700

    Initial commit

---
+++ android/src/dk/brams/android/nuttygame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package dk.brams.android.nuttygame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import dk.brams.android.nuttygame.NuttyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NuttyGame(), config);
+	}
+}
